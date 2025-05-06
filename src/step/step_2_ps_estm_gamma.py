Fixed precision error on step 2 , now all the values of step 2 after 6 to 7 precision is matching with matlab

src/step/step_2_ps_estm_gamma.py
@@ -246,11 +246,13 @@ def step_2_ps_estm_gamma(workdir:str,patch:str,parms:dict) -> None:
                 Prand = filtered / np.sum(gauss_win)
                 Prand = Prand[window_len:]
 
-                input_array = np.concatenate([[1], Prand])
-                x_orig = np.arange(len(input_array))
-                x_interp = np.linspace(0, len(input_array)-1, len(input_array)*10)
-                interpolated = np.interp(x_interp, x_orig, input_array)
-                Prand = interpolated[:-9]
+                R = 10
+                fp = np.concatenate(([1.0], Prand))
+                N = len(fp)
+                xp = np.arange(N, dtype=np.float64)
+                num_points = (N - 1) * R + 1
+                x = np.linspace(xp[0], xp[-1],num=num_points)
+                Prand = np.interp(x, xp, fp)
 
                 indices = np.round(coh_ps * 1000).astype(int) + 1
                 indices = np.clip(indices, 0, len(Prand) - 1)

src/step/step_3_ps_select.py
@@ -240,7 +240,6 @@ def step_3_ps_select(workdir:str,patch:str,parms:dict) -> None:
             ix_j = ix_j[(ix_j >= 0) & (ix_j < ph_bit.shape[1])]
             ph_bit[np.ix_(ix_i, ix_j)] = 0
 
-            ph_filt = np.zeros_like(ph_bit)
             for i_ifg in range(n_ifg):
                 ph_filt[:, :, i_ifg] = clap_filt_patch(ph_bit[:, :, i_ifg], clap_alpha, clap_beta, pm['low_pass'])
             ph_patch2[i, :] = ph_filt[ps_bit_i, ps_bit_j, :]
@@ -277,7 +276,7 @@ def step_3_ps_select(workdir:str,patch:str,parms:dict) -> None:
 
     bperp_range = max(bperp) - min(bperp)
     keep_ix = (coh_ps2 > coh_thresh) & (abs(pm["K_ps"][ix].flatten() - K_ps2) < 2*np.pi/bperp_range)
-
+    print(f"{len(keep_ix)} ps selected after re-estimation of coherence")
     if np.sum(keep_ix) == 0:
         print("***No PS points left. Updating the stamps log for this****")
     