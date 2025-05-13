a5d1fb4a6068490b3f396ffc36eeff8ef636e3ac
Rahul Sharan <rahul.sharan@korrai.com>
2025-05-13 16:16:24 +0530

    Removed unwanted code in step 5

--- src/step/step_5_ps_merge.py
+++ src/step/step_5_ps_merge.py
@@ -42,10 +42,8 @@ def correct_phase(patch_dir:str,psver:int):
 def intersect_rows(a, b):
     a_view = a.view([('', a.dtype)] * a.shape[1])
     b_view = b.view([('', b.dtype)] * b.shape[1])
-
     _, IA, IB = np.intersect1d(a_view, b_view, return_indices=True)
-    C = a[IA] if len(IA) > 0 else np.empty((0, a.shape[1]), dtype=a.dtype)    
-    return C, IA, IB
+    return IA, IB
 
 def estimate_noise_stddev(workdir:str,psver:int):
     psname = f'ps{psver}.h5'
@@ -111,9 +109,6 @@ def step_5_ps_merge(workdir:str,parms:dict):
     phname = f"ph{psver}.h5"
     rcname = f"rc{psver}.h5"
     pmname = f"pm{psver}.h5"
-    phuwname = f"phuw{psver}.h5"
-    sclaname = f"scla{psver}.h5"
-    scnname = f"scn{psver}.h5"
     bpname = f"bp{psver}.h5"
     laname = f"la{psver}.h5"
     hgtname = f"hgt{psver}.h5"
@@ -133,16 +128,11 @@ def step_5_ps_merge(workdir:str,parms:dict):
     ph = np.zeros((0, 0), dtype=float)
     ph_rc = np.zeros((0, 0), dtype=float)
     ph_reref = np.zeros((0, 0), dtype=float)
-    ph_uw = np.zeros((0, 0), dtype=np.float32)
     ph_patch = np.zeros((0, 0), dtype=float)
     ph_res = np.zeros((0, 0), dtype=float)
-    ph_scla = np.zeros((0, 0), dtype=np.float32)
-    ph_scn_slave = np.zeros((0, 0), dtype=float)
     K_ps = np.zeros((0, 0), dtype=float)
     C_ps = np.zeros((0, 0), dtype=float)
     coh_ps = np.zeros((0, 0), dtype=float)
-    K_ps_uw = np.zeros((0, 0), dtype=np.float32)
-    C_ps_uw = np.zeros((0, 0), dtype=np.float32)
     bperp_mat = np.zeros((0, 0), dtype=np.float32)
     la = np.zeros((0, 0), dtype=float)
     hgt = np.zeros((0, 0), dtype=float)
@@ -154,10 +144,6 @@ def step_5_ps_merge(workdir:str,parms:dict):
         correct_phase(os.path.join(workdir,patch),psver)
 
         ps = read_h5(os.path.join(workdir,patch, psname))
-        if "n_image" in ps:
-            n_image = int(ps["n_image"])
-        else:
-            n_image = int(ps["n_ifg"])
 
         # Load patch information and identify points in the patch
         patch_ij = np.loadtxt(os.path.join(workdir,patch, 'patch_noover.in'))
@@ -167,9 +153,9 @@ def step_5_ps_merge(workdir:str,parms:dict):
             (ps['ij'][:, 2] >= patch_ij[0] - 1) & 
             (ps['ij'][:, 2] <= patch_ij[1] - 1))
 
-        C, IA, IB = intersect_rows(ps['ij'][ix, 1:3], ij)
+        IA, IB = intersect_rows(ps['ij'][ix, 1:3], ij)
         remove_ix = np.concatenate([remove_ix, IB])
-        C, IA, IB = intersect_rows(ps['ij'][:,1:3], ij)
+        IA, IB = intersect_rows(ps['ij'][:,1:3], ij)
         ix_ex = np.ones(ps['n_ps'], dtype=bool)
         ix_ex[IA] = False
         ix[ix_ex] = 1
@@ -203,7 +189,6 @@ def step_5_ps_merge(workdir:str,parms:dict):
             coh_ps = np.vstack(pm['coh_ps'][ix]) if coh_ps.size == 0 else np.vstack([coh_ps, pm['coh_ps'][ix]])
 
         bp = read_h5(os.path.join(workdir,patch, bpname))
-
         bperp_mat = np.vstack(bp['bperp_mat'][ix, :]) if bperp_mat.size == 0 else np.vstack([bperp_mat, bp['bperp_mat'][ix, :]])
         
         if os.path.exists(os.path.join(workdir,patch, laname)):
@@ -214,51 +199,6 @@ def step_5_ps_merge(workdir:str,parms:dict):
             hgtin = read_h5(os.path.join(workdir,patch, hgtname))
             hgt = np.vstack(hgtin['hgt'][ix]) if hgt.size == 0 else np.vstack([hgt, hgtin['hgt'][ix]])
 
-        if os.path.exists(os.path.join(workdir,patch, phuwname)):
-            phuw = read_h5(os.path.join(workdir,patch, phuwname))
-            phuw_data = phuw['ph_uw']
-            
-            if C is not None and len(C) > 0:
-                ph_uw_diff = np.mean(phuw_data[IA,:] - phuw_data[IB,:], axis=0)
-                ph_uw_diff = np.round(ph_uw_diff / (2 * np.pi)) * (2 * np.pi)
-            else:
-                ph_uw_diff = np.zeros(phuw_data.shape[1], dtype='single')
-            new_rows = phuw_data[ix,:] - np.tile(ph_uw_diff, (np.sum(ix), 1))
-            ph_uw = np.vstack([ph_uw, new_rows]) if ph_uw.size > 0 else new_rows
-        else:
-            zeros_to_append = np.zeros((np.sum(ix), n_image), dtype='single')
-            ph_uw = np.vstack([ph_uw, zeros_to_append]) if ph_uw.size > 0 else zeros_to_append
-        
-        if os.path.exists(os.path.join(workdir,patch, sclaname)):
-            scla = read_h5(os.path.join(workdir,patch, sclaname))
-            scla_ph_scla = scla['ph_scla']
-            scla_K_ps_uw = scla['K_ps_uw']
-            scla_C_ps_uw = scla['C_ps_uw']
-            if C is not None and len(C) > 0:
-                ph_scla_diff = np.mean(scla_ph_scla[IA,:] - ph_scla[IB,:], axis=0)
-                K_ps_diff = np.mean(scla_K_ps_uw[IA,:] - K_ps_uw[IB,:])
-                C_ps_diff = np.mean(scla_C_ps_uw[IA,:] - C_ps_uw[IB,:])
-            else:
-                ph_scla_diff = np.zeros(scla_ph_scla.shape[1], dtype='single')
-                K_ps_diff = 0
-                C_ps_diff = 0
-            new_ph_scla = scla_ph_scla[ix,:] - np.tile(ph_scla_diff, (np.sum(ix), 1))
-            new_K_ps_uw = scla_K_ps_uw[ix,:] - np.tile(K_ps_diff, (np.sum(ix), 1))
-            new_C_ps_uw = scla_C_ps_uw[ix,:] - np.tile(C_ps_diff, (np.sum(ix), 1))
-            
-            ph_scla = np.vstack([ph_scla, new_ph_scla]) if ph_scla.size > 0 else new_ph_scla
-            K_ps_uw = np.vstack([K_ps_uw, new_K_ps_uw]) if K_ps_uw.size > 0 else new_K_ps_uw
-            C_ps_uw = np.vstack([C_ps_uw, new_C_ps_uw]) if C_ps_uw.size > 0 else new_C_ps_uw
-        
-        if os.path.exists(os.path.join(workdir,patch, scnname)):
-            scn = read_h5(os.path.join(workdir,patch, scnname))
-            scn_ph_scn_slave = scn['ph_scn_slave']
-            if C is not None and len(C) > 0:
-                ph_scn_diff = np.mean(scn_ph_scn_slave[IA,:] - ph_scn_slave[IB,:], axis=0)
-            else:
-                ph_scn_diff = np.zeros(scn_ph_scn_slave.shape[1], dtype='single')
-            new_ph_scn_slave = scn_ph_scn_slave[ix,:] - np.tile(ph_scn_diff, (np.sum(ix), 1))
-            ph_scn_slave = np.vstack([ph_scn_slave, new_ph_scn_slave]) if ph_scn_slave.size > 0 else new_ph_scn_slave
     # Loop Ended through patch directories
     
     n_ps_orig = ij.shape[0]
@@ -273,7 +213,6 @@ def step_5_ps_merge(workdir:str,parms:dict):
     dups_indices = np.setdiff1d(np.arange(lonlat.shape[0]), unique_indices)
     keep_ix_num = np.where(keep_ix)[0]
 
-    
     # Process duplicates
     for dup_index in dups_indices:
         dups_ix_weed = np.where(
@@ -327,10 +266,6 @@ def step_5_ps_merge(workdir:str,parms:dict):
     ph_reref = ph_reref[sort_ix, :]
     save_h5(workdir, rcname , **{'ph_rc': ph_rc, 'ph_reref': ph_reref})
 
-    if ph_uw.shape[0] == n_ps_orig:
-        ph_uw = ph_uw[sort_ix, :]
-        save_h5(workdir, phuwname , **{'ph_uw': ph_uw})
-
     ph_patch = ph_patch[sort_ix, :]
     ph_res = ph_res[sort_ix, :] if ph_res.shape[0] == n_ps_orig else np.array([])
     K_ps = K_ps[sort_ix, :] if K_ps.shape[0] == n_ps_orig else np.array([])
@@ -338,18 +273,7 @@ def step_5_ps_merge(workdir:str,parms:dict):
     coh_ps = coh_ps[sort_ix, :] if coh_ps.shape[0] == n_ps_orig else np.array([])
     save_h5(workdir, pmname , **{'ph_patch': ph_patch, 'ph_res': ph_res,
                                 'K_ps': K_ps, 'C_ps': C_ps,"coh_ps":coh_ps})
-
-    if ph_scla.shape[0] == n_ps:
-        ph_scla = ph_scla[sort_ix, :]
-        K_ps_uw = K_ps_uw[sort_ix, :]
-        C_ps_uw = C_ps_uw[sort_ix, :]
-        save_h5(workdir, sclaname , **{'ph_scla': ph_scla, 'K_ps_uw': K_ps_uw,
-                                      'C_ps_uw': C_ps_uw})
-    
-    if ph_scn_slave.shape[0] == n_ps:
-        ph_scn_slave = ph_scn_slave[sort_ix, :]
-        save_h5(workdir, scnname , **{'ph_scn_slave': ph_scn_slave})
-    
+        
     ph = ph[sort_ix, :] if ph.shape[0] == n_ps_orig else np.array([])
     save_h5(workdir, phname , **{'ph': ph})
     
